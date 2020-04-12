40859b1592ca750b71ac10a6cdd7a98a7cc0dd2e
lilianzheng <lilianzheng@yy.com>
2020-04-12 22:00:48 +0800

    feat(view cyce): uiview的加载与展示的几个相关生命周期 1. init 2. willMoveToSuperView, didMoveSuperView  UIview添加到父view上，也就是例子中的self.view。每个viewController自带一个默认的view 3. willMoveToWindow, didMoveToWindow UIView随父view一起添加到window上，然后view会在页面上显示出来

--- iosStepByStep_oc/ViewController.m
+++ iosStepByStep_oc/ViewController.m
@@ -12,11 +12,45 @@
 
 @end
 
+@interface TestView : UIView
+
+@end
+
+@implementation TestView
+
+- (UIView*) init {
+    self = [super init];
+    if(self) {
+        
+    }
+    return self;
+}
+
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview {
+    [super willMoveToSuperview:newSuperview];
+}
+- (void)didMoveToSuperview {
+    [super didMoveToSuperview];
+}
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow {
+    [super willMoveToWindow:newWindow];
+}
+- (void)didMoveToWindow {
+    [super didMoveToWindow];
+}
+
+@end
+
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    UIView* testView = [[TestView alloc] init];
+    testView.backgroundColor = [UIColor redColor];
+    testView.frame = CGRectMake(100, 100, 100, 100);
+    
+    [self.view addSubview:testView];
 }
 
 
